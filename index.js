b05ca63f219ff9993c06d8292feeba32ad810adc
Shaik Sirajuddin <153270051+Sirajspidy02@users.noreply.github.com>
2025-03-22 16:24:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get('/login', (req, res) => {
 // })
 
 app.listen(process.env.port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port`)
   })
