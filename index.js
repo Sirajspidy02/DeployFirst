45144e863635bd0bb464b731c3bd47c2dc8f9366
Shaik Sirajuddin <153270051+Sirajspidy02@users.noreply.github.com>
2025-03-22 16:12:03 +0530

    First

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config()
+// import express from "express"
+const app = express()
+// const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send("Got it")
+})
+
+app.get('/login', (req, res) => {
+    res.send("Got it")
+})
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
